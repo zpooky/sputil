f5cd018f36701026cb0040bb7c8ac1f52628d935
Fredrik Olsson <spooky.bender@gmail.com>
2018-02-12 22:57:13 +0100

    more random test

--- test/prng/randomTest.cpp
+++ test/prng/randomTest.cpp
@@ -4,7 +4,7 @@
 #include <prng/xorshift.h>
 
 #define test(r)                                                                \
-do {                                                                           \
+  do {                                                                         \
     for (std::uint32_t inc = 0; inc < 100; ++inc) {                            \
       for (std::uint32_t ex = inc + 1; ex < 100; ++ex) {                       \
         bool first = false;                                                    \
@@ -20,19 +20,18 @@ do {                                                                           \
         ASSERT_FALSE(last);                                                    \
       }                                                                        \
     }                                                                          \
-  }                                                                            \
-  while (0)
+  } while (0)
 
-#define test_uniform_one(r) \
-do {\
-  for(std::size_t i=0;i<160;++i){ \
-    using TTT = typename decltype(r)::Word;\
-    TTT start(0);\
-    TTT end(1);\
-    auto res = uniform_dist(r,start,end); \
-    ASSERT_EQ(res,TTT(0)); \
-  }\
-} while(0)
+#define test_uniform_one(r)                                                    \
+  do {                                                                         \
+    for (std::size_t i = 0; i < 160; ++i) {                                    \
+      using TTT = typename decltype(r)::Word;                                  \
+      TTT start(0);                                                            \
+      TTT end(1);                                                              \
+      auto res = uniform_dist(r, start, end);                                  \
+      ASSERT_EQ(res, TTT(0));                                                  \
+    }                                                                          \
+  } while (0)
 
 TEST(randomTest, test_xorshift32) {
   prng::Xorshift32 r(1);
@@ -52,4 +51,14 @@ TEST(randomTest, test_xorshift128plus) {
 TEST(randomTest, test_xorshift128plus_dist_limited) {
   prng::Xorshift128plus r(1, 1);
   test_uniform_one(r);
+}
+
+TEST(randomTest, ll) {
+  prng::Xorshift32 r(1);
+  for (std::size_t i = 0; i < 10000; ++i) {
+    printf("%u,", random(r));
+    if (i % 10 == 0) {
+      printf("\n");
+    }
+  }
 }
