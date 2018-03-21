9d6e80030d1a8fe33f2f938945e0a39a24cd391a
Fredrik Olsson <spooky.bender@gmail.com>
2018-03-21 15:17:22 +0100

    btree dump

--- Makefile
+++ Makefile
@@ -10,6 +10,7 @@ BUILD_DIR = build/debug
 HEADER_DIRS = -I$(INCLUDE_DIR)
 # overrides makes it possible to externally append extra flags
 override CXXFLAGS += $(HEADER_DIRS) -enable-frame-pointers -std=c++14 -Wall -Wextra -Wpedantic -Wpointer-arith -Wconversion -Wshadow
+override CXXFLAGS += -Wduplicated-cond -Wlogical-op -Wnull-dereference -Wuseless-cast -Wdouble-promotion -Wformat=2
 CXXFLAGS_DEBUG = $(CXXFLAGS) -ggdb
 LDFLAGS =
 LDLIBS =
--- src/tree/btree.h
+++ src/tree/btree.h
@@ -131,6 +131,8 @@ is_empty(const BTNode<T, K> &tree) noexcept {
 template <typename T, std::size_t K>
 static void
 copy_split(BTNode<T, K> *left, std::size_t sep, BTNode<T, K> *right) noexcept {
+  assert(left);
+  assert(right);
   assert(is_empty(*right));
   assert(left->parent == right->parent);
 
@@ -140,7 +142,7 @@ copy_split(BTNode<T, K> *left, std::size_t sep, BTNode<T, K> *right) noexcept {
   const std::size_t start = sep + 1;
   {
     /*
-     * Move elements from the right of the median to the right subtree
+     * Move elements from @ the right of the median to the right subtree
      */
     std::size_t moved = 0;
     auto &elems = left->elements;
@@ -314,6 +316,170 @@ insert_node(BTNode<T, k> *tree, V &&val, std::size_t index,
   }
 }
 
+template <std::size_t keys>
+std::size_t
+dump_levels(BTNode<int, keys> *root, std::size_t acum = 0) noexcept {
+  std::size_t result = acum;
+  if (root) {
+    acum += 1;
+    result = acum;
+    auto &c = root->children;
+    for (std::size_t i = 0; i < length(c); ++i) {
+      result = std::max(result, dump_levels(c[i], acum));
+    }
+  }
+  return result;
+}
+std::size_t
+dump_colums_for(std::size_t children, std::size_t level) {
+  if (level == 0) {
+    return 1;
+  } else {
+    return children * dump_colums_for(children, level - 1);
+  }
+}
+
+void
+dump_print_null_column() {
+  printf("[XXXXX]");
+}
+
+template <typename T>
+static void
+dump_print_column(T *root) noexcept {
+  printf("[");
+  for (std::size_t i = 0; i < capacity(root->elements); ++i) {
+    if (i != 0) {
+      printf("|");
+    }
+    if (i < length(root->elements)) {
+      printf("%02d", root->elements[i]);
+    } else {
+      printf("__");
+    }
+  }
+  printf("]");
+}
+
+inline std::size_t
+dump_max_node_children(std::size_t children, std::size_t level) {
+  return dump_colums_for(children, level);
+}
+
+template <std::size_t keys>
+inline void
+dump_do_add_children(BTNode<int, keys> *root,
+                     sp::StaticArray<BTNode<int, keys> *, 128> &result) {
+  auto &children = root->children;
+  std::size_t i = 0;
+  for (; i < length(children); ++i) {
+    assert(insert(result, children[i]));
+  }
+  for (; i < capacity(children); ++i) {
+    assert(insert(result, nullptr));
+  }
+}
+
+template <std::size_t keys>
+void
+dump_get_columns_for(BTNode<int, keys> *root, std::size_t level,
+                     sp::StaticArray<BTNode<int, keys> *, 128> &result,
+                     std::size_t orig_level) {
+
+  if (root == nullptr) {
+    std::size_t empties = dump_max_node_children(keys + 1, orig_level);
+    for (std::size_t i = 0; i < empties; ++i) {
+      assert(insert(result, nullptr));
+    }
+  } else if (level == 0) {
+    dump_do_add_children(root, result);
+  } else {
+    auto &children = root->children;
+    std::size_t i = 0;
+    for (; i < length(children); ++i) {
+      dump_get_columns_for(children[i], level - 1, result, orig_level);
+    }
+    for (; i < capacity(children); ++i) {
+      dump_get_columns_for<keys>(nullptr, std::size_t(level - 1), result,
+                                 orig_level);
+    }
+  }
+}
+inline std::size_t
+dump_calc_width(std::size_t elements, std::size_t max_elements) {
+  // printf("elements[%zu] <= max_elements[%zu]\n", elements, max_elements);
+  assert(elements <= max_elements);
+  return (max_elements / elements);
+}
+
+template <typename F>
+void
+dump_print_width(std::size_t width, F f) {
+  std::size_t pre_post = (width - 1) / 2;
+
+  for (std::size_t i = 0; i < pre_post; ++i) {
+    printf("       ");
+  }
+  f();
+  for (std::size_t i = 0; i < pre_post; ++i) {
+    printf("       ");
+  }
+}
+
+template <std::size_t keys>
+void
+dump_print(BTNode<int, keys> *root,
+           sp::StaticArray<BTNode<int, keys> *, 128> &columns) {
+
+  auto width = dump_calc_width(
+      length(columns), dump_colums_for(keys + 1, dump_levels(root) - 1));
+  for (std::size_t i = 0; i < length(columns); ++i) {
+    dump_print_width(width, [&] {
+      if (columns[i]) {
+        dump_print_column(columns[i]);
+      } else {
+        dump_print_null_column();
+      }
+    });
+  }
+  printf("\n");
+}
+
+template <std::size_t keys>
+void
+dump_level(BTNode<int, keys> *root, std::size_t level) noexcept {
+
+  sp::StaticArray<BTNode<int, keys> *, 128> columns;
+  // insert(columns, root);
+  dump_get_columns_for(root, level, columns, level);
+  dump_print(root, columns);
+}
+
+template <std::size_t keys>
+void
+dump(BTNode<int, keys> *root) noexcept {
+  if (root) {
+    auto lvls = dump_levels(root);
+
+    {
+      sp::StaticArray<BTNode<int, keys> *, 128> columns;
+      insert(columns, root);
+      dump_print(root, columns);
+    }
+    for (std::size_t lvl = 0; lvl < lvls - 1; ++lvl) {
+      dump_level(root, lvl);
+    }
+  }
+  // // printf("levels[%zu]\n", lvls);
+  // if (root) {
+  //   print_print_column(root);
+  //   // dump_children(root);
+  //   for_each(root->children, [](auto c) { //
+  //     dump(c);
+  //   });
+  // }
+}
+
 } // namespace btree
 } // namespace impl
 
@@ -438,23 +604,6 @@ find(Tree<T, k, C> &tree, const K &needle) noexcept {
 
 template <typename T>
 static void
-dump_elements(T *root) noexcept {
-  printf("[");
-  for (std::size_t i = 0; i < capacity(root->elements); ++i) {
-    if (i != 0) {
-      printf("|");
-    }
-    if (i < length(root->elements)) {
-      printf("%02d", root->elements[i]);
-    } else {
-      printf("__");
-    }
-  }
-  printf("]\n");
-}
-
-template <typename T>
-static void
 dump_children(T *root) noexcept {
   int height = 0;
   height = reduce(root->children, height, [](auto h, auto c) {
@@ -471,21 +620,10 @@ dump_children(T *root) noexcept {
   }
 }
 
-template <std::size_t keys>
-void
-dump(btree::impl::btree::BTNode<int, keys> *root) noexcept {
-  if (root) {
-    dump_elements(root);
-    // dump_children(root);
-    for_each(root->children, [](auto c) { //
-      dump(c);
-    });
-  }
-}
-
 template <std::size_t k, typename C>
 void
 dump(const Tree<int, k, C> &tree) noexcept {
+  using namespace btree::impl::btree;
   dump(tree.root);
 }
 
