6fb95e801e17fdd8a6b184f42df7145c7946c7c7
Fredrik Olsson <spooky.bender@gmail.com>
2018-04-13 16:35:31 +0200

    simple queue

---
+++ src/queue/Queue.h
@@ -0,0 +1,92 @@
+#ifndef SP_UTIL_QUEUE_QUEUE_H
+#define SP_UTIL_QUEUE_QUEUE_H
+
+#include <list/LinkedList.h>
+#include <memory/Allocator.h>
+
+namespace sp {
+template <typename T, template <typename> class Allocator = sp::Allocator>
+struct LinkedListQueue {
+  /**/
+  LinkedList<T, Allocator> impl;
+  LinkedListQueue() noexcept;
+
+  LinkedListQueue(const LinkedListQueue &) = delete;
+  LinkedListQueue(LinkedListQueue &&) noexcept;
+
+  LinkedListQueue &
+  operator=(const LinkedListQueue &) = delete;
+  LinkedListQueue &
+  operator=(const LinkedListQueue &&) = delete;
+};
+
+//=====================================
+template <typename T, template <typename> class A, typename V>
+T *
+enqueue(LinkedListQueue<T> &, V &&) noexcept;
+//=====================================
+template <typename T, template <typename> class A>
+bool
+dequeue(LinkedListQueue<T> &, T &out) noexcept;
+//=====================================
+template <typename T, template <typename> class A>
+bool
+is_empty(const LinkedListQueue<T> &) noexcept;
+//=====================================
+template <typename T, template <typename> class A>
+std::size_t
+length(const LinkedListQueue<T> &) noexcept;
+//=====================================
+
+/*
+ * =======================================================
+ */
+template <typename T, template <typename> class A>
+LinkedListQueue<T, A>::LinkedListQueue() noexcept
+    : impl() {
+}
+
+template <typename T, template <typename> class A>
+LinkedListQueue<T, A>::LinkedListQueue(LinkedListQueue &&o) noexcept
+    : impl(std::move(o.impl)) {
+}
+
+//=====================================
+template <typename T, template <typename> class A, typename V>
+T *
+enqueue(LinkedListQueue<T, A> &queue, V &&val) noexcept {
+  return push_back(queue.impl, std::forward<V>(val));
+}
+//=====================================
+template <typename T, template <typename> class A>
+bool
+dequeue(LinkedListQueue<T, A> &queue, T &out) noexcept {
+  T *head = get(queue.impl, 0);
+  if (head) {
+    // using sp::swap;
+    using std::swap;
+    swap(*head, out);
+    assert(remove_first(queue.impl, //
+                        [](const auto &) { return true; }));
+    return true;
+  }
+
+  return false;
+}
+//=====================================
+template <typename T, template <typename> class A>
+bool
+is_empty(const LinkedListQueue<T, A> &queue) noexcept {
+  return is_empty(queue.impl);
+}
+//=====================================
+template <typename T, template <typename> class A>
+std::size_t
+length(const LinkedListQueue<T, A> &queue) noexcept {
+  return queue.length;
+}
+
+//=====================================
+}
+
+#endif
